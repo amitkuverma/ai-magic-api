10a7d3bd82cc0555daa45050c4b9f2584ad1f423
AMIT VERMA <amit.verma@softobotics.com>
2024-11-02 20:56:57 +0530

    code

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ import Transaction from './models/user/transaction.model';
 import AiEarning from './models/user/ai-earning.model';
 import aiRouter from './routes/aiEarning.routes';
 import dailyRouter from './routes/dailyEarning.routes';
+import DailyEarning from './models/user/daily-earning.model';
 
 const app: Application = express();
 
@@ -37,7 +38,17 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.put('/api/update-earnings', async (req, res) => {
+app.put('/api/ai-earnings', async (req, res) => {
+  try {
+    await updateAIEarningsForAllUsers();
+    res.status(200).json({ message: 'Daily earnings updated successfully' });
+  } catch (error) {
+    console.error('Error updating daily earnings:', error);
+    res.status(500).json({ message: 'Error updating daily earnings' });
+  }
+});
+
+app.put('/api/daily-earnings', async (req, res) => {
   try {
     await updateDailyEarningsForAllUsers();
     res.status(200).json({ message: 'Daily earnings updated successfully' });
@@ -46,9 +57,8 @@ app.put('/api/update-earnings', async (req, res) => {
     res.status(500).json({ message: 'Error updating daily earnings' });
   }
 });
-// Adjusted autoCreateDailyEarnings function
-// Assuming Payment is a Sequelize model
-async function updateDailyEarningsForAllUsers() {
+
+async function updateAIEarningsForAllUsers() {
   try {
     // Fetch all users from the Payment table
     const existingUsers = await Payment.findAll();
@@ -93,8 +103,57 @@ async function updateDailyEarningsForAllUsers() {
     console.error('Error updating daily earnings for all users:', error);
   }
 }
+// Adjusted autoCreateDailyEarnings function
+// Assuming Payment is a Sequelize model
+async function updateDailyEarningsForAllUsers() {
+  try {
+    // Fetch all users from the Payment table
+    const existingUsers = await Payment.findAll();
+
+    for (const user of existingUsers) {
+      // Check if the user has a commission value
+      const commission = parseFloat(user.commission);
+      if (commission) {
+        console.log(commission)
+        console.log((user.selfInvestment * commission) / 100)
+        // Calculate the new earnings value
+        const newEarnings = parseFloat(user.earnWallet.toString()) + (user.selfInvestment * commission) / 100;
+        const aiEarnings = parseFloat(user.aiEarning.toString()) + (user.selfInvestment * commission) / 100;
+
+        // Update the earnWallet field for each user
+        await Payment.update(
+          {
+            earnWallet: newEarnings,
+            aiEarning: aiEarnings
+          },
+          { where: { payId: user.payId } }
+        );
+        console.log({
+          userId: user.userId,
+          userName: user.userName,
+          aiEarning: aiEarnings,
+          status: 'paid'
+        })
+        await DailyEarning.create(
+          {
+            userId: user.userId,
+            userName: user.userName,
+            aiEarning: (user.selfInvestment * commission) / 100,
+            status: 'paid'
+          }
+        );
+
+        console.log(`Updated earnings for userId ${user.payId} to ${newEarnings}`);
+      }
+    }
+  } catch (error) {
+    console.error('Error updating daily earnings for all users:', error);
+  }
+}
 
 
+// Schedule the auto-increment function to run daily at 1 AM
+cron.schedule('0 0 * * *', updateAIEarningsForAllUsers);
 // Schedule the auto-increment function to run daily at 1 AM
 cron.schedule('0 0 * * *', updateDailyEarningsForAllUsers);
 // Catch-all 404 handler
